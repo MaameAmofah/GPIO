a64cc775ccd4dc0cfdacdc238ecebb9efc88ccfb
Edith Maame Yaa Amofah <maameyaaamofah@gmail.com>
2018-07-04 03:38:23 +0000

    my second file

---
+++ Edith.py
@@ -0,0 +1,158 @@
+Python 3.5.3 (default, Jan 19 2017, 14:11:04) 
+[GCC 6.3.0 20170124] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: /home/pi/My pyt6hon labs.py ====================
+>>> a = 20
+>>> while(a > 2):
+	a = a - 2
+	print (a)
+
+	
+18
+16
+14
+12
+10
+8
+6
+4
+2
+>>> 
+>>> for i in range(1,10):
+	print (i)
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> 
+>>> for j in [1,2,3]:
+	print (j)
+
+	
+1
+2
+3
+>>> 
+>>> for j in range(1,4):
+	print (j)
+
+	
+1
+2
+3
+>>> type (j)
+<class 'int'>
+>>> type (range)
+<class 'type'>
+>>> 
+>>> for j in (0,19):
+	while(j < 20)
+	
+SyntaxError: invalid syntax
+>>> 
+>>> or j in (0,19):
+	
+SyntaxError: invalid syntax
+>>> for j in (0,19):
+	print (j > 19)
+
+	
+False
+False
+>>> for jprint (j > 19)
+SyntaxError: invalid syntax
+>>> for jprint (j > 19):
+	
+SyntaxError: invalid syntax
+>>> for j in (0,19):
+	print (j)
+
+	
+0
+19
+>>> for j in range(0,19):
+	print (j < 19)
+
+	
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+>>> 
+>>> for j in range(0,20,2):
+	print (j)
+
+	
+0
+2
+4
+6
+8
+10
+12
+14
+16
+18
+>>> 
+>>> for a in [****]:
+	
+SyntaxError: invalid syntax
+>>> for a in [*,*,*,*]:
+	
+SyntaxError: invalid syntax
+>>> 
+>>> for a in [*,*,*,*]:
+	
+SyntaxError: invalid syntax
+>>> for a in [*,*,*,*]:
+	
+SyntaxError: invalid syntax
+>>> for a in [*, *, *, *]:
+	
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> def myfunction ():
+	x = str( input("type a word contianing a"):
+		 
+SyntaxError: invalid syntax
+>>> def myfunction ():
+
+
+
+
+
+
+	
+KeyboardInterrupt
+>>> def isvowel(char):
+	
