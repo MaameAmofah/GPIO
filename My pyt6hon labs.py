9f1773134dd5b53f50a1865f8b16ba6b03f0f8a0
Edith Maame Yaa Amofah <maameyaaamofah@gmail.com>
2018-07-09 09:09:58 +0000

    tweepy files

--- My pyt6hon labs.py
+++ My pyt6hon labs.py
@@ -1,1 +1,18 @@
 import math
+math.radians(32)
+
+#printing out the surface area and volume of a sphere.
+
+def radius():
+    radius = int(input("radius"))
+    area = (math.pi * radius * radius)
+    volume = (2 * math.pi * radius)
+    answers = {"area" : area, "volume" : volume}
+    print (answers)
+
+# splitting a sentence into its words
+
+sentence = ("where are you" ,"ana" , "kofi")
+
+print (sentence)
+
